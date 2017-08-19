3b8a8e4c88273e1e9cf9a1fa3d80193ba6d30dd6
ahmedbeheiri <ahmedbeheirii@gmail.com>
2017-08-19 21:05:49 +0200

    editing in webview

--- app/src/main/java/com/ahmed_beheiri/kitcheninphone/RecipeWay.java
+++ app/src/main/java/com/ahmed_beheiri/kitcheninphone/RecipeWay.java
@@ -3,6 +3,7 @@ package com.ahmed_beheiri.kitcheninphone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -12,12 +13,15 @@ import butterknife.ButterKnife;
 public class RecipeWay extends AppCompatActivity {
     @BindView(R.id.webview)
     WebView webView;
+    @BindView(R.id.toolbar_actionbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipeway);
         ButterKnife.bind(this);
+        setupToolbar(getString(R.string.app_name));
         Intent i = getIntent();
         String sourceurl = i.getStringExtra("sourceurl");
         webView.getSettings().setJavaScriptEnabled(true);
@@ -25,4 +29,9 @@ public class RecipeWay extends AppCompatActivity {
         webView.loadUrl(sourceurl);
 
     }
+    private void setupToolbar(String title) {
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+    }
+
 }
